9a73db489a9aec03dbe6ca2a7114be17ff97f42b
Frances <s1136610@student.hsleiden.nl>
2024-01-11 18:30:14 +0100

    Log out functionality.

---
+++ src/app/account-menu/account-menu.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import {Router} from "@angular/router";
+import {UserService} from "../services/user-service";
+
+@Component({
+  selector: 'app-account-menu',
+  templateUrl: './account-menu.component.html',
+  styleUrls: ['./account-menu.component.scss']
+})
+export class AccountMenuComponent {
+  @Output() logout: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  onLogoutClick() {
+    this.userService.destroyJWT();
+    this.userService.destroyUser();
+    this.router.navigate(['/']);
+  }
+}
